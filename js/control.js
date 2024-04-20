0c514744cc37991b96c149fa995f5aef6d8edc2b
Sam Bardsley <bardsls@wwu.edu>
2024-04-20 13:52:30 -0700

    Initial commit

---
+++ js/control.js
@@ -0,0 +1,109 @@
+// each stylesheet needs info about when it should be turned on
+class Sheet {
+    constructor(name, whiteList, blackList, extraSheets) {
+        this.name = name;
+        this.whiteList = whiteList;
+        this.blackList = blackList;
+        this.extraSheets = extraSheets;
+    }
+  
+    appliesTo(currentUrl) {
+        for (let i = 0; i < this.whiteList.length; i++) {
+            if (this.whiteList[i].test(currentUrl)) return true;
+        }
+
+        for (let i = 0; i < this.blackList.length; i++) {
+            if (this.blackList[i].test(currentUrl)) return false;
+        }
+
+        return true;
+    }
+
+    loadExtraSheets() {
+        for (let i = 0; i < this.extraSheets.length; i++) {
+            loadCSS(this.extraSheets[i]);
+        }
+    }
+
+    unloadExtraSheets() {
+        for (let i = 0; i < this.extraSheets.length; i++) {
+            unloadCSS(this.extraSheets[i]);
+        }
+    }
+}
+
+// all the possible stylesheets to be turned on
+const sheets = [
+    new Sheet("main_page", 
+        [new RegExp("^.*://.*\\.reddit\\.com/r/all.*$"),
+        new RegExp("^.*://.*\\.reddit\\.com/r/popular.*$"),
+        new RegExp("^.*://.*\\.reddit\\.com.$")], 
+        [new RegExp("^.*://.*\\.reddit\\.com/r.*$")],
+        []),
+    new Sheet("subreddits", [], 
+        [new RegExp("^.*://.*\\.reddit\\.com/r/all.*$"),
+        new RegExp("^.*://.*\\.reddit\\.com/r/popular.*$"),
+        new RegExp("^.*://.*\\.reddit\\.com.$"),
+        new RegExp("^.*://.*\\.reddit\\.com/\\?.*$")],
+        ["leftsidebar"]),
+    new Sheet("trending", [], [], []),
+    new Sheet("notifications", [], [], [])
+]; // for future use: new RegExp("^.*://.*\\.reddit\\.com/.*$") = all pages
+// add reddit/search
+
+initialize();
+
+function initialize() {
+    var currentUrl = window.location.href;
+    // accesses preferences from storage to determine style sheets
+    chrome.storage.sync.get(null, function(data) {
+        sheets.forEach((sheet, index) => {
+            console.log(sheet.name);
+            if (!data[index]) {
+                unloadCSS(sheet.name);
+                sheet.unloadExtraSheets();
+            } else if (!sheet.appliesTo(currentUrl)) {
+                unloadCSS(sheet.name);
+                sheet.loadExtraSheets();
+            }
+            else {
+                loadCSS(sheet.name);
+                sheet.loadExtraSheets();
+            }
+        });
+    });
+}
+
+// adds css file to doc
+function loadCSS(file) {
+    var link = document.createElement("link");
+    link.href = chrome.runtime.getURL('css/reddit/' + file + '.css');
+    link.id = file;
+    console.log(link.href);
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("html")[0].appendChild(link);
+}
+
+// removes css file from doc
+function unloadCSS(file) {
+    var cssNode = document.getElementById(file);
+    cssNode && cssNode.parentNode.removeChild(cssNode);
+}
+
+// reloads upon message from extension's popup
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.action === 'reloadCSS') {
+        initialize();
+    }
+});
+
+// reinitializing when navigating within website
+let lastUrl = location.href; 
+new MutationObserver(() => {
+  const url = location.href;
+  if (url !== lastUrl) {
+    lastUrl = url;
+    initialize();
+  }
+}).observe(document, {subtree: true, childList: true});
---
+++ js/options.js
@@ -0,0 +1,54 @@
+const numOfOptions = 4;
+const checks = new Array(numOfOptions).fill(false);
+
+document.addEventListener("DOMContentLoaded", initialize);
+document.querySelector("form").addEventListener("change", saveOptions);
+
+function initialize() {
+    chrome.storage.sync.get(null, function(data) {
+        for (let i = 0; i < numOfOptions; i++) {
+            if (data[i+""] !== undefined) {
+                checks[i] = data[i+""];
+                /*document.getElementsByClassName(".slider").style.transition = "0s";*/
+                document.getElementById(i+"").checked = checks[i];
+                /*document.getElementsByClassName(".slider").style.transition = ".3s";*/
+            }
+        }
+        updateImage();
+    });
+}
+
+function saveOptions(e) {
+    // toggles check
+    checks[e.target.id] = !checks[e.target.id];
+    updateImage();
+
+    // updates browser storage
+    const data = {};
+    for (let i = 0; i < numOfOptions; i++) {
+        data[i+""] = checks[i];
+    }
+    chrome.storage.sync.set(data);
+    e.preventDefault();
+
+    // reload page if changes are applied
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, {
+        action: 'reloadCSS',
+        });
+    });
+}
+
+function updateImage() {
+    for (let i = 0; i < numOfOptions; i++) {
+        if (checks[i]) {
+            document.getElementById("c1").style.borderColor = "#FF5700";
+            document.getElementById("c1").style.backgroundImage = 'url("../icons/control_orange.svg")';
+            document.getElementById("title").style.color = "#FF5700";
+            return;
+        }
+    }
+    document.getElementById("c1").style.borderColor = "#272727";
+    document.getElementById("c1").style.backgroundImage = 'url("../icons/control_grey.svg")';
+    document.getElementById("title").style.color = "#272727";
+}
